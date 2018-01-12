18faddc17645ee4d401097e164796b166ee9b9fc
Dmytro <winerr22@gmail.com>
2018-01-12 20:45:23 +0200

    change second task

--- src/main/java/lecture4/StringTask.java
+++ src/main/java/lecture4/StringTask.java
@@ -25,24 +25,35 @@ public class StringTask {
     public String rightTab(){
         StringBuilder resultText = new StringBuilder();
         char[] textArray = text.toCharArray();
-        char [][] exitText = new char[textArray.length/50+10][51];
+        char [][] exitText = new char[textArray.length/51+5][51];
         int index = 0;
         int row = 0, column;
+        int startPosition;
         for (int i=50; i<textArray.length; i+=50){
             while (true) {
                 if (textArray[i] == ' ' || textArray[i] == '\n') {
                     column = 50-(i-index);
+                    startPosition = column;
                     for (int l=0; l<column; l++)
                         exitText[row][l] = ' ';
                     for ( ; index < i; index++) {
                         if(textArray[index] == '\n'){
+                            //переробити! має переставляти з кінця
+                            int size = 50;
+                            for(int j=column-1; j>=startPosition; j--){
+                                //int k = 50 - column + j;
+                                exitText[row][size] = exitText[row][j];
+                                exitText[row][j] = ' ';
+                                size--;
+                            }
+
                             index++;
                             i = index;
                             break;
                         }
                         exitText[row][column] = textArray[index];
                         column++;
-                    }
+                    }                                                                                                                                                                                                                   
                     exitText[row][column] = '\n';
                     row++;
                     break;
